4cdfdbb285ebd1b70ce50bc08ee63473c4cddecf
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-09-29 03:20:10 +0300

    Add : swipe

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// offsetWidth - это ширина блока, включая все padding, border, но без margin.
-
-// clientWidth - это ширина блока, включая padding, но без border, margin и без scroll. Если блоки инлайновые, то ширина блока будет равна 0.
-
-// getBoundingClientRect().width - это ширина блока высчитывается по принципу clientWidth, но даёт дробное число.
-
+// offsetWidth                      - это ширина блока, включая все padding, border, но без margin.
+// clientWidth                      - это ширина блока, включая padding, но без border, margin и без scroll. Если блоки инлайновые, то ширина блока будет равна 0.
+// getBoundingClientRect().width    - это ширина блока, высчитывается по принципу clientWidth, но даёт дробное число.
 // document.querySelector('.myDiv').offsetWidth
 // document.querySelector('.myDiv').clientWidth
 // document.querySelector('.myDiv').getBoundingClientRect().width
 
 const slider = document.querySelector(".slider");
-const sliderline = document.querySelector(".slider-line");
+const sliderLine = document.querySelector(".slider-line");
 const sliderItems = document.querySelectorAll(".slider-item");
-
+//------------------------------------------------------------
 const bntLeft = document.querySelector(".bnt-left");
 const bntRight = document.querySelector(".bnt-right");
 
 let counnt = 0;
+
+// --- main ---
 
 resetSize();
 
@@ -28,6 +27,8 @@ bntLeft.addEventListener("click", () => {
 bntRight.addEventListener("click", () => {
     right();
 });
+
+// --- main ---
 
 // --- functions ---
 
@@ -98,6 +99,7 @@ function mouveLine() {
     //------- no break point mouve -----------
 
     console.log(`function mouveline : counnt = ${counnt}`);
+
     if (counnt < 0) {
         counnt = getMaxCounnt();
     }
@@ -108,7 +110,7 @@ function mouveLine() {
 
     //------- /no break point mouve -----------
 
-    sliderline.style.transform = `translateX(-${getWidthItem() * counnt}px)`;
+    sliderLine.style.transform = `translateX(-${getWidthItem() * counnt}px)`;
 }
 
 function left() {
@@ -122,5 +124,73 @@ function right() {
 }
 
 // --- /mouve ---
+//
+// ---  swipe ---
+
+let posInit = 0;
+let posX1 = 0;
+let posX2 = 0;
+
+let trfRegExp = /[-0-9.]+(?=px)/;
+
+slider.addEventListener("mousedown", swipeStart);
+slider.addEventListener("touchstart", swipeStart);
+
+function getEvent(event) {
+    // return evetn for touch or mouse to .clientX
+    return event.type.search("touch") !== -1 ? event.touches[0] : event;
+}
+
+function swipeStart(event) {
+    let e = getEvent(event);
+    posInit = posX1 = e.clientX; // first coordinate X axis
+
+    sliderLine.style.transition = ""; // remove the smooth transition
+
+    // отслеживать другие события на документе
+    document.addEventListener("touchmove", swipeAction);
+    document.addEventListener("touchend", swipeEnd);
+    document.addEventListener("mousemove", swipeAction);
+    document.addEventListener("mouseup", swipeEnd);
+}
+
+function swipeAction(event) {
+    let e = getEvent(event);
+    let style = sliderLine.style.transform; // получаем занчение transform в формате 'translateX(0px)'
+    let transform = +style.match(trfRegExp)[0]; // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+
+    posX2 = posX1 - e.clientX; // получаем разницу start и mouve
+
+    posX1 = e.clientX; // текущее положение в x1
+
+    sliderLine.style.transform = `translateX(${transform - posX2}px)`; // двигаем sliderLine
+}
+
+function swipeEnd() {
+    console.log(`swipeEnd()`);
+    // финальная позиция курсора
+    posFinal = posInit - posX1;
+
+    document.removeEventListener("touchmove", swipeAction);
+    document.removeEventListener("mousemove", swipeAction);
+    document.removeEventListener("touchend", swipeEnd);
+    document.removeEventListener("mouseup", swipeEnd);
+
+    if (Math.abs(posFinal) > getMaxCounnt()) {         // убираем знак минус и сравниваем с порогом сдвига слайда
+        if (posInit < posX1) {                       // если мы тянули вправо, то уменьшаем номер текущего слайда
+            -- counnt;
+            // если мы тянули влево, то увеличиваем номер текущего слайда
+        } else if (posInit > posX1) {
+            ++ counnt;
+        }
+    }
+
+    // если курсор двигался, то запускаем функцию переключения слайдов
+    if (posInit !== posX1) {
+       mouveLine() ;
+    }
+}
+
+// --- /swipe ---
 
 // --- /functions ---
